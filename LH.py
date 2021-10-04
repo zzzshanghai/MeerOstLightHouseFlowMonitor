0b971e1bb619966e2a9e46c5c31c1d09b972e772
zzzshanghai <9117406+zzzshanghai@users.noreply.github.com>
2021-10-04 22:47:09 +0800

    Update LH.py

--- LH.py
+++ LH.py
@@ -15,7 +15,7 @@ gaojinSatus="告警状态"
 SecretId = os.environ["SecretId"]
 SecretKey = os.environ["SecretKey"]
 
-regions = ["ap-beijing", "ap-chengdu", "ap-guangzhou", "ap-hongkong", "ap-nanjing", "ap-shanghai", "ap-singapore", "ap-tokyo", "eu-moscow", "na-siliconvalley"]
+regions = ["ap-hongkong"]
 percent = 0.80  # 流量限额，1表示使用到100%关机，默认设置为95%
 
 #key参数  
