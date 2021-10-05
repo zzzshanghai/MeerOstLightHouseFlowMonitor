c8bc37c1b680d4e3e120d85ef4c408279000729a
zzzshanghai <9117406+zzzshanghai@users.noreply.github.com>
2021-10-05 22:43:22 +0800

    Update LH.py

--- LH.py
+++ LH.py
@@ -2,7 +2,6 @@ import json
 import time
 import requests
 import os
-import yagmail
 from tencentcloud.common import credential
 from tencentcloud.common.profile.client_profile import ClientProfile
 from tencentcloud.common.profile.http_profile import HttpProfile
@@ -15,9 +14,6 @@ gaojinSatus="告警状态"
 
 SecretId = os.environ["SecretId"]
 SecretKey = os.environ["SecretKey"]
-SmtpUser = os.environ["SmtpUser"]
-SmtpPassword = os.environ["SmtpPassword"]
-SmtpReceiver = os.environ["SmtpReceiver"]
 
 regions = ["ap-hongkong"]
 percent = 0.80  # 流量限额，1表示使用到100%关机，默认设置为95%
@@ -106,10 +102,6 @@ def dofetch(id, key, region):
         #添加时间戳
         print (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), "（该时间较北京时间晚8小时）")
         print ("---------------------------------------------")
-        
-        gaojin = gaojinData+gaojinResult+gaojinSatus
-        yag = yagmail.SMTP(user=SmtpUser, password=SmtpPassword, host='smtp.163.com')
-        yag.send(to=SmtpReceiver, subject='腾讯云轻量应用服务器流量告警', contents=gaojin)
 
 #except TencentCloudSDKException as err: 
  #   print(err) 
